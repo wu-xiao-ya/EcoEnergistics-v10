5b2ae3658cb01528593aeb4ef65099bb84affe35
兀小雅 <zpyn1818@outlook.com>
2026-07-12 00:38:27 +0800

    Fix explicit imports and remove Mekanism 9 jar

--- src/main/java/aeternal/ecoenergistics/common/block/BlockEcoTransmitter.java
+++ src/main/java/aeternal/ecoenergistics/common/block/BlockEcoTransmitter.java
@@ -40,6 +40,7 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
